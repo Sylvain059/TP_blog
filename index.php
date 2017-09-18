04f78c5bc3e0038bdd7e4f304a1ee5030623d663
Sylvain059 <sylvain.sculier88@gmail.com>
2017-09-18 17:15:01 +0200

    index.php // add html tag

--- index.php
+++ index.php
@@ -25,5 +25,15 @@ $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%
 while ($donnees = $req->fetch())
 {
 ?>
+<article class="news">
+
+    <h3><?php echo htmlspecialchars($donnees['titre']); ?><em>le <?php echo $donnees['date_creation_fr']; ?></em></h3>
+    <p>
+    <?php
+    	echo nl2br(htmlspecialchars($donnees['contenu']));
+    ?><br/>
+    <em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
+    </p>
+</article>
 </body>
 </html>
