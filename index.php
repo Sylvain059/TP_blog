81dc5b26592577b4c7753fcea7d3ff54f3e378dd
Sylvain059 <sylvain.sculier88@gmail.com>
2017-09-18 17:11:09 +0200

    index.php / add loop for message

--- index.php
+++ index.php
@@ -10,21 +10,20 @@
 	<p>Derniers articles...</p>
 		<!-- connexion à la base données -->
 	<?php 
-	try
-
+try
 {
-
-    $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'Skaoune88');
-
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 }
 
 catch(Exception $e)
-
 {
-
-        die('Erreur : '.$e->getMessage());
-
+   die('Erreur : '.$e->getMessage());
 }
-	 ?>
+
+$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
+while ($donnees = $req->fetch())
+{
+?>
 </body>
 </html>
