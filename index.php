b38233c7e1f9ee05a33c9035653c88da90f5c80e
Sylvain059 <sylvain.sculier88@gmail.com>
2017-09-18 17:06:43 +0200

    index.php / add link cononection bdd

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>Blog PHP</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+	<h1>Mon blog</h1>
+	<p>Derniers articles...</p>
+		<!-- connexion à la base données -->
+	<?php 
+	try
 
+{
+
+    $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'Skaoune88');
+
+}
+
+catch(Exception $e)
+
+{
+
+        die('Erreur : '.$e->getMessage());
+
+}
+	 ?>
 </body>
 </html>
