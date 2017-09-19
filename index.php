ad68211ebae7a3e71c06ad97675b423d9f14656f
Sylvain059 <sylvain.sculier88@gmail.com>
2017-09-19 08:55:18 +0200

    index.php / add comment

--- index.php
+++ index.php
@@ -12,21 +12,19 @@
 	<?php 
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'Skaoune88');
 }
-
 catch(Exception $e)
 {
    die('Erreur : '.$e->getMessage());
 }
 
 $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
+// Boucle PHP
 while ($donnees = $req->fetch())
 {
 ?>
 <article class="news">
-
     <h3><?php echo htmlspecialchars($donnees['titre']); ?><em>le <?php echo $donnees['date_creation_fr']; ?></em></h3>
     <p>
     <?php
@@ -35,5 +33,10 @@ while ($donnees = $req->fetch())
     <em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
     </p>
 </article>
+<?php
+
+} 
+$req->closeCursor();
+?>
 </body>
 </html>
